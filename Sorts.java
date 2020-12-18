c78ad272daa1e96975f9c11245d912c04594a8ec
Logan <lruzzier20@stuy.edu>
2020-12-17 20:19:34 -0500

    classwork

--- Sorts.java
+++ Sorts.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
 public class Sorts{
   /**Bubble sort of an int array.
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void main(String[] args){
-
+  public static void bubbleSort(int[] data){
+    int temp=0;
+    int swaps=1;
+    while(swaps>0){
+      swaps=0;
+      for(int i=0;i+1<data.length;i++){
+        if(data[i]>data[i+1]){temp=data[i]; data[i]=data[i+1]; data[i+1]=temp; swaps++;}
+      }
+    }
   }
-  
-  public static void bubbleSort(int[] data){  }
 }
