d5c28e26ea6ea94909e72d540274f866e09c79aa
Logan <lruzzier20@stuy.edu>
2020-12-17 20:39:05 -0500

    I should probably keep track of this

--- Sorts.java
+++ Sorts.java
@@ -7,15 +7,17 @@ public class Sorts{
   public static void bubbleSort(int[] data){
     int temp=0;
     int swaps=1;
+    int limit=data.length;
     while(swaps>0){
       swaps=0;
-      for(int i=0;i+1<data.length;i++){
+      for(int i=0;i+1<limit;i++){
         if(data[i]>data[i+1]){
           temp=data[i];
           data[i]=data[i+1];
           data[i+1]=temp;
           swaps++;
           System.out.println(Arrays.toString(data));
+          if(i+2==limit){limit--; System.out.println("limit is now "+limit);}
         }
       }
     }
