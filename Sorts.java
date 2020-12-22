80bf60dbea7a489d4cc45b93770144e44b3a569d
Logan <lruzzier20@stuy.edu>
2020-12-21 22:23:15 -0500

    added insertionSort() DEBUG

--- Sorts.java
+++ Sorts.java
@@ -21,7 +21,10 @@ public class Sorts{
       }
     }
   }
-
+  /**selection sort of an int array.
+   *@postcondition The array will be modified such that the elements will be in increasing order.
+   *@param data - the elements to be sorted.
+   */
   public static void selectionSort(int[] data){
     int tempValue=2147483647;
     int tempIndex=0;
@@ -34,4 +37,20 @@ public class Sorts{
       tempValue=2147483647;
     }
   }
+  /**insertion sort of an int array.
+   *@postcondition The array will be modified such that the elements will be in increasing order.
+   *@param data - the elements to be sorted.
+   */
+  public static void insertionSort(int[] data){
+    int tempV=0;
+    int temppV=0;
+    boolean notdone=true;
+    for(int i=1;i<data.length;i++){
+      tempV=data[i];
+      for(int j=i-1;j-1>=-1&&notdone;j--){
+        if(data[j]>tempV){temppV=data[j]; data[j]=tempV; data[j+1]=temppV; System.out.println(Arrays.toString(data));}else{notdone=false; System.out.println(Arrays.toString(data));}
+      }
+    }
+  }
+
 }
